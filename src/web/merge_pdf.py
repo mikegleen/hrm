6bc42303680a5303e6970435ba8c61df060a8f96
Mike Gleen <mike.gleen@gmail.com>
2026-03-30 15:05:11 +0100

    remove deprecated

--- src/web/merge_pdf.py
+++ src/web/merge_pdf.py
@@ -4,24 +4,36 @@ Merge multiple PDF files into a single file.
 The input files must all be in the same directory. The files will be sorted by
 filename.
 
-Parameters:
-    1. Directory name containing files to be merged.
-    2. Output file name.
-
-
 """
+import argparse
 import os
 from PyPDF2 import PdfMerger
 import sys
 
-VERBOS = 1
+
+def getparser():
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('indir', help='''The input directory to contain the PDF files.
+    ''')
+    parser.add_argument('outfile', help='''The output PDF file to contain the merged PDF files.
+    ''')
+    parser.add_argument('-v', '--verbose', type=int, default=1, help='''Set the verbosity.
+The default is 1 which prints summary information.
+        ''')
+    return parser
+
+
+def getargs(argv):
+    parser = getparser()
+    args = parser.parse_args(args=argv[1:])
+    return args
 
 
 def main(indir, outfilename):
     output = PdfMerger()
     for pdfname in sorted(os.listdir(indir)):
         pdfpath = os.path.join(indir, pdfname)
-        if VERBOS > 0:
+        if _args.verbose > 0:
             print(pdfpath)
         if os.path.isdir(pdfpath):
             continue
@@ -34,5 +46,9 @@ def main(indir, outfilename):
 
 
 if __name__ == '__main__':
-    assert sys.version_info >= (3, 8)
-    main(sys.argv[1], sys.argv[2])
+    assert sys.version_info >= (3, 9)
+    called_from_sphinx = False
+    if len(sys.argv) == 1:
+        sys.argv.append('-h')
+    _args = getargs(sys.argv)
+    main(_args.indir, _args.outfile)
