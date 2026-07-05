be1fa08a6170abf63f991010cd6886906db267e0
Mike Gleen <mike.gleen@gmail.com>
2026-07-05 10:07:17 +0100

    Add blank page if odd pages.

--- src/web/merge_pdf.py
+++ src/web/merge_pdf.py
@@ -7,15 +7,19 @@ filename.
 """
 import argparse
 import os
-from PyPDF2 import PdfMerger
+from PyPDF2 import PdfMerger, PdfReader
 import sys
 
+BLANKFILEPATH = 'data/blankpage.pdf'
 
 def getparser():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('indir', help='''The input directory to contain the PDF files.
     ''')
     parser.add_argument('outfile', help='''The output PDF file to contain the merged PDF files.
+    ''')
+    parser.add_argument('-e', '--even', action='store_true',  help='''Add a blank page to each input file
+    if the page count is odd.
     ''')
     parser.add_argument('-v', '--verbose', type=int, default=1, help='''Set the verbosity.
 The default is 1 which prints summary information.
@@ -29,19 +33,26 @@ def getargs(argv):
     return args
 
 
-def main(indir, outfilename):
+def main(indir: str, outfilename):
     output = PdfMerger()
     for pdfname in sorted(os.listdir(indir)):
         pdfpath = os.path.join(indir, pdfname)
-        if _args.verbose > 0:
+        if _args.verbose > 0 and not _args.even:
             print(pdfpath)
         if os.path.isdir(pdfpath):
             continue
-        leadpart, extension = os.path.splitext(pdfname)
-        if extension.lower() != '.pdf':
+        if not pdfpath.lower().endswith('.pdf'):
             print(f'Ignoring {pdfname}')
             continue
         output.append(pdfpath)
+        if _args.even:
+            with open(pdfpath, 'rb') as pdffile:
+                doc = PdfReader(pdffile)
+                pages = len(doc.pages)
+                if _args.verbose > 0:
+                    print(f'{pdfpath}     {pages=}')
+                if pages % 2 == 1:  # if odd
+                    output.append(BLANKFILEPATH)
     output.write(outfilename)
 
 
